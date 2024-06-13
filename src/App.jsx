84b18fef9b052c5bd58d4e213dfa790652dad113
Rohan23045 <118437754+Rohan23045@users.noreply.github.com>
2024-06-13 15:01:35 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <AddTodo />
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
